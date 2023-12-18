e6a891abd270a8ac4ffa541068b5ebabb7022444
kt-imimr <kt@imimr.net>
2023-12-19 01:49:52 +0800

    finished crud and auth

--- base/views.py
+++ base/views.py
@@ -1,9 +1,35 @@
-from django.shortcuts import render
+# from urllib import request
+from django.http import HttpResponseRedirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Task
+
+# auth
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
+
+from django.contrib.auth import logout
+
+
+class CustomLoginView(LoginView):
+    template_name = 'base/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tasks')
+    
+class CustomLogoutView(LogoutView):
+    def logout_view(request):
+        logout(request)
+        return HttpResponseRedirect(reverse_lazy('login'))
+
+    def get_success_url(self):
+        return reverse_lazy('login')
+    # HttpResponseRedirect(self.get_success_url())
+
 
 
 class TaskList(ListView):
@@ -19,4 +45,13 @@ class TaskCreate(CreateView):
     model = Task
     fields = '__all__'
     success_url = reverse_lazy('tasks')
-    template_name = 'base/create_form.html'
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class DeleteView(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')
